39b57240b1a9c92b5bb27ca3ab577b0d2df6787f
raviteja7723 <155376735+raviteja7723@users.noreply.github.com>
2024-01-02 20:53:38 +0530

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,15 @@
+public class BankAccount {
+    private float balance;
+
+    public BankAccount(float balance) {
+        this.balance = balance;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+}
